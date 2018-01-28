b866d4751ea4811480f83d544a8142d2fed25bc7
zain khalid <zain.khalid@post.harvard.edu>
2018-01-27 23:49:01 -0500

    First cut of functional drag and drop capability

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Dropzone from "react-dropzone"
 
 export default class EditorLayout extends React.Component {
 
@@ -17,6 +18,21 @@ export default class EditorLayout extends React.Component {
     this.props.specActions.updateSpec(newYaml)
   }
 
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    if (rejectedFiles.length > 0) {
+      alert("Please drag and drop one (and only one) .yaml or .json swagger spec file.")
+    } else if (acceptedFiles.length === 1) {
+      const file = acceptedFiles[0]
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        const spec = reader.result
+        this.onChange(spec)
+      }
+
+      reader.readAsText(file, "utf-8")
+    }
+  }
+
   render() {
     let { getComponent } = this.props
 
@@ -25,18 +41,37 @@ export default class EditorLayout extends React.Component {
     let Container = getComponent("Container")
     let EditorContainer = getComponent("EditorContainer", true)
     const SplitPaneMode = getComponent("SplitPaneMode", true)
-
+ 
     return (
       <div>
         <Container className='container'>
-          <SplitPaneMode>
-            <EditorContainer
-              onChange={this.onChange}
-              />
-            <UIBaseLayout/>
-        </SplitPaneMode>
-      </Container>
-    </div>
+          <Dropzone
+            className="dropzone"
+            accept=".yaml,application/json"
+            multiple={false}
+            onDrop={this.onDrop}
+            disablePreview
+            disableClick
+          >
+          {({ isDragActive }) => {
+            if (isDragActive) {
+              return (
+                <div className="dropzone__overlay">
+                  Please drop a .yaml or .json swagger spec.
+                </div>
+              )
+            } else {
+              return (
+                <SplitPaneMode>
+                  <EditorContainer onChange={this.onChange} />
+                  <UIBaseLayout/>
+                </SplitPaneMode>
+              )
+            }
+          }}
+          </Dropzone>
+        </Container>
+      </div>
 
   )
   }
